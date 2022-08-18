06368c2a5bdb675f691dfedda0c36d1672b940ae
Prakash Kandel <prakash.kandel@sensand.com>
2022-08-18 19:54:52 +1000

    Add token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,12 @@ import { environment } from '../environments/environment';
     HttpClientModule
   ],
   providers: [
-    ...environment.providers
+    ...environment.providers,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+     },
   ],
   bootstrap: [AppComponent]
 })
